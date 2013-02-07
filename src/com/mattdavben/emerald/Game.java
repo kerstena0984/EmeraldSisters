0bb6117e80c6d6f3a0e5344fcef680bafa5a171b
Matt Bennett <Matt@MattDavBen.com>
2013-02-06 19:23:03 -0500

    Drew shells for important methods in Game class.

--- src/com/mattdavben/emerald/Game.java
+++ src/com/mattdavben/emerald/Game.java
@@ -6,15 +6,46 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas {
+public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private static final int HEIGHT = 320;
 	private static final int WIDTH = HEIGHT * 16 / 10;
 	private static final byte SCALE = 2;
 	private static final String NAME = "Emerald Sisters | Pre-Alpha 0.0.1";
-	
-	public static void main(String[] argv){
+
+	private boolean running = false;
+
+	public Game() {
+
+	}
+
+	private void start() {
+		running = true;
+		new Thread(this).start();
+	}
+
+	private void stop() {
+		running = false;
+	}
+
+	private void init() {
+
+	}
+
+	private void update() {
+
+	}
+
+	private void render() {
+
+	}
+
+	public void run() {
+
+	}
+
+	public static void main(String[] argv) {
 		Game game = new Game();
 		game.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
 		game.setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
@@ -28,6 +59,7 @@ public class Game extends Canvas {
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-	}
 
+		game.start();
+	}
 }
