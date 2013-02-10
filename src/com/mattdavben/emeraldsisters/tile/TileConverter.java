4bebe6deb95edc65622eecc091d7c6fff0028937
Matt Bennett <Matt@MattDavBen.com>
2013-02-10 17:02:50 -0500

    Drew and rendered closed chests.

--- src/com/mattdavben/emeraldsisters/tile/TileConverter.java
+++ src/com/mattdavben/emeraldsisters/tile/TileConverter.java
@@ -8,6 +8,8 @@ public class TileConverter {
 			return new GroundTile(1);
 		case 0xA0A0A0:
 			return new SolidTile(1, 2);
+		case 0x7D110B:
+			return new SolidTile(1, 3);
 		default:
 			return new GroundTile(0);
 		}
