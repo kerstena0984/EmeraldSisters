ac1f9942c2b3871ed38d159950eb755f89d10835
Matt Bennett <Matt@MattDavBen.com>
2013-04-03 13:44:29 -0400

    Added Battles to Main. No way to switch between. YET!

--- src/com/mattdavben/emeraldsisters/Main.java
+++ src/com/mattdavben/emeraldsisters/Main.java
@@ -1,28 +1,26 @@
 package com.mattdavben.emeraldsisters;
 
 import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.state.StateBasedGame;
 
 import com.google.common.eventbus.Subscribe;
+import com.mattdavben.emeraldsisters.battle.Battle;
+import com.mattdavben.emeraldsisters.world.World;
 
-public class Main extends StateBasedGame implements StateChangeListener {
+public class Main extends BasicGame implements StateChangeListener {
 
 	private final static String NAME = "Emerald Sisters | Pre-Alpha 0.0.2";
-	public static final int play = 1;
 	public static final int GAME_SCREEN_WIDTH = 800;
 	public static final int GAME_SCREEN_HEIGHT = 600;
+	private World world;
+	private Battle battle;
 
 	public Main(String name) {
 		super(name);
-		this.addState(new PlayState(play));
 		EventNexus.register(this);
-	}
-
-	@Override
-	public void initStatesList(GameContainer container) throws SlickException {
-		this.enterState(play);
 	}
 
 	public static void main(String[] argv) {
@@ -39,7 +37,26 @@ public class Main extends StateBasedGame implements StateChangeListener {
 
 	@Subscribe
 	public void listen(StateChangeEvent event) {
-		this.enterState(event.getState());
+		// this.enterState(event.getState());
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics gr) throws SlickException {
+		world.render(gc, gr);
+		// battle.render(gc, gr);
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+		world = new World(gc.getInput());
+		// battle = new Battle.BattleBuilder().WithCharacter(new
+		// BattleEntity("Katherine")).build();
+	}
+
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException {
+		world.update(gc, delta);
+
 	}
 
 }
